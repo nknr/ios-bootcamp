739bd0525e2bfc1e3eaf541a1b049943306d0b36
Neeraj Kumar <nk88337@gmail.com>
2025-01-12 22:12:25 +0530

    first commit for ios learning

---
+++ ios-bootcamp/ios_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_bootcampApp.swift
+//  ios-bootcamp
+//
+//  Created by Neeraj Kumar on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
